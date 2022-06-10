fd3e94b968370784f59421cb1f5b20da094446ac
yogeshmagarde <106680684+yogeshmagarde@users.noreply.github.com>
2022-06-10 17:08:11 +0530

    Add files via upload

---
+++ JOBPORTAL/jobapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from.models import Registration,Job,candidate,HrReg,Job1,Image,Resume
+admin.site.register(Registration)
+admin.site.register(Job)
+admin.site.register(candidate)
+admin.site.register(HrReg)
+admin.site.register(Job1)
+admin.site.register(Resume)
+admin.site.register(Image)
+
+# Register your models here.
---
+++ JOBPORTAL/jobapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from. import views
+urlpatterns=[
+    path('registration',views.registration,name='registration'),
+    path('login',views.login,name='login'),
+    path('Logout',views.Logout,name='Logout'),
+    path('job',views.job,name='job'),
+    path('Candidate',views.Candidate,name='Candidate'),
+    path('hrreg',views.hrreg,name='hrreg'),
+    path('hrlogin',views.hrlogin,name='hrlogin'),
+    path('insertjob',views.insertjob,name='insertjob'),
+    path('resume',views.resume,name='resume'),
+    path('dash',views.dash,name='dash'),
+    path('show',views.show,name='show'),
+   
+   
+]
